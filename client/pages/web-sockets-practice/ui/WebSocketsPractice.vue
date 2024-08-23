de5d9ca69455d380c5f3e279b000f8efe157825a
awesomex <bestawes0mex@gmail.com>
2024-08-23 21:59:52 +0500

    ws

--- client/pages/web-sockets-practice/ui/WebSocketsPractice.vue
+++ client/pages/web-sockets-practice/ui/WebSocketsPractice.vue
@@ -2,16 +2,22 @@
   import { useWebSocket } from "@vueuse/core";
 
   const { status, data, send, open, close } = useWebSocket(
-    "wss://fstream.binance.com/ws/adausdt@aggTrade"
+    "wss://fstream.binance.com/ws/adausdt@aggTrade",
+    {
+      autoReconnect: true,
+    }
   );
-  const { data: btcData } = useWebSocket("wss://fstream.binance.com/ws/btcusdt@aggTrade");
-  console.log(status);
+  const { data: btcData } = useWebSocket("wss://fstream.binance.com/ws/btcusdt@aggTrade", {
+    autoReconnect: true,
+  });
+  const btcTicker = computed(() => JSON.parse(btcData.value));
+  const btcPrice = computed(() => JSON.parse(btcData.value));
 </script>
 
 <template>
   <h1>Тут будут веб сокеты</h1>
-  <h1>{{ JSON.parse(data).s }} : {{ JSON.parse(data).p }}</h1>
-  <h1>{{ JSON.parse(btcData).s }} : {{ JSON.parse(btcData).p }}</h1>
+  <h1>{{ btcTicker.s }} : {{ btcPrice.p }}</h1>
+  <h1>{{}} : {{}}</h1>
   <UiButton @click="">Send</UiButton>
 </template>
 
