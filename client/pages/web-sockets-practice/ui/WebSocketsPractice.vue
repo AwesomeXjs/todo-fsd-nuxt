1b8a7b7c271dbc6ca44e526a93fcf25448d1340e
awesomex <bestawes0mex@gmail.com>
2024-08-23 22:26:41 +0500

    create ws for all pars

--- client/pages/web-sockets-practice/ui/WebSocketsPractice.vue
+++ client/pages/web-sockets-practice/ui/WebSocketsPractice.vue
@@ -2,22 +2,30 @@
   import { useWebSocket } from "@vueuse/core";
 
   const { status, data, send, open, close } = useWebSocket(
-    "wss://fstream.binance.com/ws/adausdt@aggTrade",
+    "wss://fstream.binance.com/ws/!markPrice@arr@1s",
     {
       autoReconnect: true,
     }
   );
-  const { data: btcData } = useWebSocket("wss://fstream.binance.com/ws/btcusdt@aggTrade", {
-    autoReconnect: true,
-  });
-  const btcTicker = computed(() => JSON.parse(btcData.value));
-  const btcPrice = computed(() => JSON.parse(btcData.value));
+  const allMarket = computed(() => JSON.parse(data.value));
 </script>
 
 <template>
   <h1>Тут будут веб сокеты</h1>
-  <h1>{{ btcTicker.s }} : {{ btcPrice.p }}</h1>
-  <h1>{{}} : {{}}</h1>
+  <div v-if="status === 'CONNECTING'">Loading...</div>
+  <div v-else-if="status === 'CLOSED'">Соединения разорвано</div>
+  <div v-else>
+    <div class="flex flex-wrap items-center">
+      <div
+        class="mx-1 my-2 inline-block flex items-center border-2 p-2"
+        v-for="ticker in allMarket"
+      >
+        <h3 class="mr-2">Ticker: {{ ticker.s }}</h3>
+        <h3>Price: {{ ticker.p }}</h3>
+      </div>
+    </div>
+  </div>
+
   <UiButton @click="">Send</UiButton>
 </template>
 
